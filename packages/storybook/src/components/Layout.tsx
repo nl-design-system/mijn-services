a6dd4df6789664f5c6bfc5cc38e2b3f971609711
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2025-10-30 10:11:35 +0100

    feat: consistent focus rings in themes

--- packages/storybook/src/components/Layout.tsx
+++ packages/storybook/src/components/Layout.tsx
@@ -18,7 +18,9 @@ export const Layout = ({ logo, children, className, footerLogo, ...props }: Layo
   return (
     <Root className={className} {...props}>
       <PageHeader className="todo-page-header">
-        <SkipLink href="#main">Direct naar de hoofdinhoud</SkipLink>
+        <SkipLink className="todo-skip-link" href="#main">
+          Direct naar de hoofdinhoud
+        </SkipLink>
         <Grid className="todo-page-header-content">{logo}</Grid>
       </PageHeader>
       <PageContent className="todo-page-content">{children}</PageContent>
@@ -36,9 +38,12 @@ export const Layout = ({ logo, children, className, footerLogo, ...props }: Layo
                 Contact
               </Heading>
               <address className="todo-address">
-                Bel <Link href="tel:453453">453 453</Link> (maandag tot en met vrijdag van 09.00 tot 17.00 uur) of stuur
-                een e-mail naar{' '}
-                <Link href="mailto:vragen@gemeentevoorbeeld.nl">
+                Bel{' '}
+                <Link className="todo-link--footer" href="tel:453453">
+                  453 453
+                </Link>{' '}
+                (maandag tot en met vrijdag van 09.00 tot 17.00 uur) of stuur een e-mail naar{' '}
+                <Link className="todo-link--footer" href="mailto:vragen@gemeentevoorbeeld.nl">
                   <span className="utrecht-url-data">vragen@gemeentevoorbeeld.nl</span>
                 </Link>
                 {'.'}
@@ -49,7 +54,7 @@ export const Layout = ({ logo, children, className, footerLogo, ...props }: Layo
               <Heading level={2} appearance="level-4">
                 Meer weten
               </Heading>
-              <LinkList>
+              <LinkList className="todo-link-list--footer">
                 <LinkListLink href="/">Over gemeente Voorbeeld</LinkListLink>
                 <LinkListLink href="/">Nieuwsbrief</LinkListLink>
                 <LinkListLink href="/">Social Media</LinkListLink>
@@ -61,7 +66,7 @@ export const Layout = ({ logo, children, className, footerLogo, ...props }: Layo
               <Heading level={2} appearance="level-4">
                 Over deze site
               </Heading>
-              <LinkList>
+              <LinkList className="todo-link-list--footer">
                 <LinkListLink href="/">Gebruikersvoorwaarden</LinkListLink>
                 <LinkListLink href="/">Proclaimer</LinkListLink>
                 <LinkListLink href="/">Cookieverklaring</LinkListLink>
