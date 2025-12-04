c4abf8dfcd045c9cb87ffdd0270593d455ac240f
Bryan de Jong <bryan@levitade.io>
2025-12-04 19:16:40 +0100

    feat: get repo owner and repo name from command line args

--- packages/discussions-backup-tool/src/index.ts
+++ packages/discussions-backup-tool/src/index.ts
@@ -1,16 +1,21 @@
 import { mkdir, writeFile } from 'node:fs/promises';
+import process from 'node:process';
 import { getDiscussions } from './graphql/index.js';
 import { processDiscussions } from './utils/index.js';
 
 const init = async () => {
+  const owner = process.argv[2];
+  const repo = process.argv[3];
+
+  if (!owner || !repo) {
+    throw new Error('Usage: node index.js <owner> <repo>');
+  }
+
   try {
     await mkdir('./out', { recursive: true });
   } catch {
     console.error('Could not create output directory');
   }
-
-  const owner = 'nl-design-system';
-  const repo = 'backlog';
 
   const discussions = await getDiscussions(owner, repo);
   const processedDiscussions = await processDiscussions(discussions);
