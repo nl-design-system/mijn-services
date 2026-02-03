6c0e0e6133068ba7329bb1814b65eac4a92c1b98
Tessa Viergever <112861180+TessaViergever@users.noreply.github.com>
2026-02-03 13:29:03 +0100

    feat: add next.js website for template showcase + wcag audit

--- packages/storybook/src/templates/mijn-omgeving-home/mijn-omgeving.stories.tsx
+++ packages/storybook/src/templates/mijn-omgeving-home/mijn-omgeving.stories.tsx
@@ -1,50 +1,10 @@
-import '@amsterdam/design-system-css/dist/grid/grid.css';
-import { Grid } from '@amsterdam/design-system-react';
-import { ActionSingle } from '@gemeente-denhaag/action';
-import { CaseCard } from '@gemeente-denhaag/card';
-import {
-  SideNavigationBase,
-  SideNavigationItem,
-  SideNavigationLink,
-  SideNavigationList,
-} from '@gemeente-denhaag/side-navigation';
-import { Heading } from '@nl-design-system-candidate/heading-react/css';
-import { Link } from '@nl-design-system-candidate/link-react/css';
-import { NumberBadge } from '@nl-design-system-candidate/number-badge-react/css';
-import { Paragraph } from '@nl-design-system-candidate/paragraph-react/css';
 import { Meta, StoryObj } from '@storybook/react-vite';
-import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/theme.css';
-import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
-import '@rijkshuisstijl-community/design-tokens/dist/index.css';
-import '../../themes/index.scss';
-import {
-  IconArchive,
-  IconBuildingCommunity,
-  IconChevronLeft,
-  IconChevronRight,
-  IconCurrencyEuro,
-  IconHome,
-  IconInbox,
-  IconInfoCircle,
-  IconLayoutGrid,
-  IconListCheck,
-  IconParking,
-  IconUser,
-} from '@tabler/icons-react';
-import {
-  Alert,
-  BreadcrumbNav,
-  BreadcrumbNavLink,
-  BreadcrumbNavSeparator,
-  Icon,
-} from '@utrecht/component-library-react/dist/css-module';
-import { ReactElement } from 'react';
-import { Layout } from '../../components/Layout';
+import MijnOmgevingHome from './MijnOmgevingHome';
 import { DenHaagLogo, PageHeaderLogo, VoorbeeldFooterLogo } from '../../components/Logo';
-import { mijnOmgevingPaths } from '../../components/template-navigation/mijnOmgevingPaths';
 
 const meta = {
   title: 'Templates/MijnOmgeving/Overzichtspagina',
+  component: MijnOmgevingHome,
   globals: {
     dir: 'ltr',
     lang: 'nl',
@@ -53,208 +13,36 @@ const meta = {
   parameters: {
     layout: 'fullscreen',
   },
-} satisfies Meta;
+} satisfies Meta<typeof MijnOmgevingHome>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const TemplatePage = ({ logo, footerLogo }: { logo: ReactElement; footerLogo?: ReactElement }) => (
-  <Layout logo={logo} footerLogo={footerLogo}>
-    <Grid paddingTop={'x-large'}>
-      <Grid.Cell span={{ narrow: 3, medium: 6, wide: 12 }}>
-        <Link href={'/#'} className="todo-breadcrumb--mobile">
-          <Icon>
-            <IconChevronLeft />
-          </Icon>
-          Home
-        </Link>
-
-        <BreadcrumbNav aria-labelledby="hidden-breadcrumb-header" className="todo-breadcrumb--desktop">
-          <h2 id="hidden-breadcrumb-header" hidden>
-            Kruimelpad
-          </h2>
-          <BreadcrumbNavLink href={'/#'}>Home</BreadcrumbNavLink>
-          <BreadcrumbNavSeparator>
-            <Icon>
-              <IconChevronRight />
-            </Icon>
-          </BreadcrumbNavSeparator>
-          <BreadcrumbNavLink href={'/#'} disabled current>
-            Gemeente Voorbeeld
-          </BreadcrumbNavLink>
-        </BreadcrumbNav>
-      </Grid.Cell>
-
-      <Grid.Cell span={3} className={'todo-grid-cell--hide-on-medium'}>
-        <SideNavigationBase>
-          <SideNavigationList>
-            <SideNavigationItem>
-              <SideNavigationLink href={mijnOmgevingPaths.overzicht} current>
-                <IconLayoutGrid />
-                Overzicht
-              </SideNavigationLink>
-            </SideNavigationItem>
-          </SideNavigationList>
-          <SideNavigationList>
-            <SideNavigationItem>
-              <SideNavigationLink href={mijnOmgevingPaths.taken}>
-                <IconListCheck />
-                Mijn taken
-              </SideNavigationLink>
-            </SideNavigationItem>
-            <SideNavigationItem>
-              <SideNavigationLink href={mijnOmgevingPaths.berichtenOverzicht}>
-                <IconInbox />
-                Mijn berichten <NumberBadge>2</NumberBadge>
-              </SideNavigationLink>
-            </SideNavigationItem>
-            <SideNavigationItem>
-              <SideNavigationLink href={mijnOmgevingPaths.zakenOverzicht}>
-                <IconArchive />
-                Mijn zaken
-              </SideNavigationLink>
-            </SideNavigationItem>
-          </SideNavigationList>
-          <SideNavigationList>
-            <SideNavigationItem>
-              <SideNavigationLink href="/#">
-                <IconCurrencyEuro />
-                Belastingzaken
-              </SideNavigationLink>
-            </SideNavigationItem>
-            <SideNavigationItem>
-              <SideNavigationLink href="/#">
-                <IconHome />
-                WOZ
-              </SideNavigationLink>
-            </SideNavigationItem>
-            <SideNavigationItem>
-              <SideNavigationLink href="/#">
-                <IconParking />
-                Parkeren
-              </SideNavigationLink>
-            </SideNavigationItem>
-            <SideNavigationItem>
-              <SideNavigationLink href="/#">
-                <IconBuildingCommunity />
-                Erfpacht
-              </SideNavigationLink>
-            </SideNavigationItem>
-          </SideNavigationList>
-          <SideNavigationList>
-            <SideNavigationItem>
-              <SideNavigationLink href={mijnOmgevingPaths.mijnGegevens}>
-                <IconUser />
-                Mijn gegevens
-              </SideNavigationLink>
-            </SideNavigationItem>
-          </SideNavigationList>
-        </SideNavigationBase>
-      </Grid.Cell>
-
-      <Grid.Cell span={{ narrow: 3, medium: 6, wide: 9 }}>
-        <main id="main">
-          <Alert icon={<IconInfoCircle className="utrecht-icon" />}>
-            <Heading level={1} appearance="level-3">
-              Heading
-            </Heading>
-            <Paragraph>Lorem ipsum dolor sit amet, consectetur ad * isicing elit, sed do eiusmod *</Paragraph>
-          </Alert>
-          <Heading level={2} appearance="level-1">
-            Hallo Jeroen van Drouwen
-          </Heading>
-          <Paragraph>
-            In ‘Mijn omgeving’ kunt u zelf uw persoonlijke zaken regelen wanneer het u uitkomt. U kunt bijvoorbeeld uw
-            rekeningen betalen en zien wanneer uw aanvraag klaar is.
-          </Paragraph>
-          <section>
-            <Heading level={2}>Wat moet ik regelen</Heading>
-            <Link href={mijnOmgevingPaths.taken}>Bekijk alle taken (10)</Link>
-            <ActionSingle
-              className={'todo-action--single'}
-              labels={labels}
-              link={mijnOmgevingPaths.zaakDetail}
-              now="2023-09-28T19:47:36.593Z"
-              dateTime="2023-09-30T19:47:36.593Z"
-              relativeDate={true}
-            >
-              <strong>Geef informatie voor uw aanvraag subsidie geluidisolatie</strong>
-            </ActionSingle>
-            <ActionSingle
-              className={'todo-action--single'}
-              dateTime="2023-10-02T19:47:36.593Z"
-              labels={labels}
-              link={mijnOmgevingPaths.zaakDetail}
-              now="2023-09-28T19:47:36.593Z"
-              relativeDate
-            >
-              <strong>Betaal uw parkeerbon van € 74,90 voor parkeren bij Valeriusplein</strong>
-            </ActionSingle>
-            <ActionSingle className={'todo-action--single'} link={mijnOmgevingPaths.zaakDetail} labels={labels}>
-              <strong>Verleng uw identiteitskaart</strong>
-            </ActionSingle>
-          </section>
-          <section>
-            <Heading level={2}>Mijn zaken</Heading>
-            <Link href={mijnOmgevingPaths.zakenOverzicht}>Bekijk alle zaken (15)</Link>
-            <div className={'todo-card-layout'}>
-              <CaseCard
-                title={'Aanvraag subsidie geluidsisolatie'}
-                href={mijnOmgevingPaths.zaakDetail}
-                context={'ZK-228402'}
-              />
-              <CaseCard
-                title={'Aanvraag parkeervergunning'}
-                href={mijnOmgevingPaths.zaakDetail}
-                context={'ZK-108422'}
-              />
-              <CaseCard
-                title={'Aanvraag subsidie geluidsisolatie'}
-                href={mijnOmgevingPaths.zaakDetail}
-                context={'ZK-228402'}
-              />
-              <CaseCard
-                title={'Aanvraag parkeervergunning'}
-                href={mijnOmgevingPaths.zaakDetail}
-                context={'ZK-108422'}
-              />
-            </div>
-          </section>
-        </main>
-      </Grid.Cell>
-    </Grid>
-  </Layout>
-);
-
 export const Default: Story = {
   parameters: {
     theme: 'voorbeeld-theme',
   },
-  render: () => <TemplatePage logo={<PageHeaderLogo />} footerLogo={<VoorbeeldFooterLogo />} />,
+  args: {
+    logo: <PageHeaderLogo />,
+    footerLogo: <VoorbeeldFooterLogo />,
+  },
 };
 
 export const DenHaagTheme: Story = {
   parameters: {
     theme: 'denhaag-theme',
   },
-  render: () => <TemplatePage logo={<DenHaagLogo />} />,
+  args: {
+    logo: <DenHaagLogo />,
+  },
 };
 
 export const RHCTheme: Story = {
   parameters: {
     theme: 'rhc-theme',
   },
-  render: () => <TemplatePage logo={<PageHeaderLogo />} />,
-};
-const labels = {
-  today: 'vandaag',
-  yesterday: 'gisteren',
-  before: 'vóór',
-  approachingDeadline: (daysDifference: number) => {
-    if (daysDifference === 1) {
-      return `nog ${daysDifference} dag`;
-    }
-    return `nog ${daysDifference} dagen`;
+  args: {
+    logo: <PageHeaderLogo />,
   },
 };
